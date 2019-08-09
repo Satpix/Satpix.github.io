5091a564a90f69cc2809aff13c80431762f4de70
Ахмед <kamash19@gmail.com>
2019-08-09 23:23:59 +0300

    да

---
+++ fs/js/main.js
@@ -0,0 +1,16 @@
+$(function(){
+
+	$('.slider_in, .news_slider_inner').slick({
+		nextArrow:'<button type="button" class="slick_btn slick-next"></button>',
+		prevArrow:'<button type="button" class="slick_btn slick-prev"></button>',
+		infinite:false
+	});
+	$('select').styler();
+	
+	
+
+
+
+
+
+});
