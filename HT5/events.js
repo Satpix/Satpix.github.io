0f0d8768c0064e02cc1809c8e68e4eb8cc7f69b4
Ахмед <kamash19@gmail.com>
2019-09-05 20:11:33 +0300

    ht5

---
+++ HT5/events.js
@@ -0,0 +1,73 @@
+(function(){
+	'use strict'
+
+
+	let form =document.forms.namedItem('converter'),
+		bynValueEl=form.elements.namedItem('bynValue'),
+		selectedCurrencyEl=form.elements.namedItem('selectedCurrency'),
+		outputEl=form.elements.namedItem('result'),
+		submitBtn = form.querySelector("button[type=submit]"),
+		currencyData=new Map();
+
+	selectedCurrencyEl.addEventListener('change',()=>{
+		let currencyCode=selectedCurrencyEl.value;
+		if(!currencyData.has(currencyCode)) {
+			toggleSubmit(false);
+			nbApi(`Rates/${currencyCode}?ParamMode=2`,
+			(err,currency)=>{
+				if(!err){
+					currencyData.set(currencyCode,currency);
+				}
+				toggleSubmit(true);
+				}
+			);
+		}
+	});
+	form.addEventListener('input',event=>{
+		event.preventDefault();
+		convert();
+
+		
+	});
+
+	function convert(){
+		let bynValue=bynValueEl.valueAsNumber || 0,
+		{Cur_Scale:scale, Cur_OfficialRate:rate}= currencyData.get(selectedCurrencyEl.value);
+		let result=bynValue/rate*scale;
+		outputEl.textContent=result.toFixed(2);
+			}
+
+
+	nbApi('Currencies', (error,currencies)=>{
+		if(error){
+			return;
+		}
+		fillOptions(currencies);
+		selectedCurrencyEl.dispatchEvent(new Event('change'))
+		});
+	function fillOptions(currencies){
+		selectedCurrencyEl.append(currencies.reduce((fragment,currency)=>{
+		let currencyExpireDate=new Date(currency.Cur_DateEnd);
+		if(currencyExpireDate.getTime()>=Date.now()){
+		let option=document.createElement('option');
+		option.textContent = currency.Cur_Name;
+		option.value = currency.Cur_Abbreviation;
+		fragment.append(option);
+		}
+		return fragment;
+		},document.createDocumentFragment()));
+	}
+
+	function nbApi(type,callback) {
+		let xhr=new XMLHttpRequest();
+		xhr.open('GET',`http://www.nbrb.by/API/ExRates/${type}`,true);
+		xhr.responseType= 'json';
+		xhr.onload=()=> callback(false,xhr.response);
+		xhr.onerror=()=> callback(true,null);
+		xhr.send();
+	}
+
+	function toggleSubmit(state){
+		submitBtn.disabled=typeof state==='boolean' ? !state : !submitBtn.disabled;
+	}
+})();
