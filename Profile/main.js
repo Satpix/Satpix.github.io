09eef5eebe77c7b27d32efc01c83b06cfccdbc4a
Sata <35007410+Satpix@users.noreply.github.com>
2021-08-03 23:27:05 +0300

    fix:update info
    Update data

--- Profile/main.js
+++ Profile/main.js
@@ -1,52 +1,22 @@
-
-window.onload=function(){
-    let a1=document.getElementById('button1');
-    let b1=document.getElementById('button2');
-    let c1=document.getElementById('button3');
-    let d1=document.getElementById('project1');
-    let e1=document.getElementById('project2');
-    let f1=document.getElementById('project3');
-
-    function projects(a,b,c,d,e,f){
-        a.style.color='#1293AD';
-        b.style.color='white';
-        c.style.color='white';
-        a.style.background='white';
-        b.style.background="linear-gradient(90deg,#05325D, #40e0f2)";
-        c.style.background="linear-gradient(90deg,#05325D, #40e0f2)";
-        d.style.display='block';
-        e.style.display='none';
-        f.style.display='none';
-        document.getElementById('project4').style.display='none';
-
+    let arr=[];
+    let arr2=[];
+    for (let i = 0; i < 4; i++){
+        arr[i]=document.getElementById('button'+(i+1));
     }
-    button1.onclick=()=>{
-        let a=a1;
-        let b=b1;
-        let c=c1;
-        let d=d1;
-        let e=e1;
-        let f=f1;
-        projects(a,b,c,d,e,f);
+    for (let i = 0; i < 4; i++){
+        arr2[i]=document.getElementById('project'+(i+1));
     }
-    button2.onclick=()=>{
-        let b=a1;
-        let a=b1;
-        let c=c1;
-        let d=e1;
-        let e=d1;
-        let f=f1;
-        projects(a,b,c,d,e,f);
-
-
+    let projects=(a)=>{
+        for(let i = 0;i<arr.length;i++){
+            if(a==arr[i]){
+                arr[i].style.color = '#1293AD';
+                arr[i].style.background = 'white';
+                arr2[i].style.display = 'block';
+            }else{
+                arr[i].style.background = "linear-gradient(90deg,#05325D, #40e0f2)";
+                arr[i].style.color = 'white';
+                arr2[i].style.display = 'none';
+            }
+        }
+        document.getElementById('project').style.display = 'none';
     }
-    button3.onclick=()=>{
-       let b=a1;
-       let c=b1;
-       let a=c1;
-       let d=f1;
-       let e=e1;
-       let f=d1;
-       projects(a,b,c,d,e,f);
-   }
-}
